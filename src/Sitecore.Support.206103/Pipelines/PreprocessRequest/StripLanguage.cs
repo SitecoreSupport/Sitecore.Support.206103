5473bc8377b56bde010d1e9c1cbbb065306e01bc
SC-IDO <ido@sitecore.net>
2018-07-04 17:45:31 +0300

    Original functionality

---
+++ src/Sitecore.Support.206103/Pipelines/PreprocessRequest/StripLanguage.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.Globalization;
+using Sitecore.Pipelines.PreprocessRequest;
+using System.Globalization;
+
+namespace Sitecore.Support.Pipelines.PreprocessRequest
+{
+  public class StripLanguage : Sitecore.Pipelines.PreprocessRequest.StripLanguage
+  {
+    protected override bool IsValidForStrippingFromUrl([NotNull] Language language, [NotNull]PreprocessRequestArgs args)
+    {
+      var cultureInfo = language.CultureInfo;
+
+      var isLocal_custom_unspecified = cultureInfo.LCID == 0x1000;
+
+      var customCulture = isLocal_custom_unspecified || cultureInfo.CultureTypes.HasFlag(CultureTypes.UserCustomCulture);
+
+      return !customCulture;
+    }
+  }
+}
